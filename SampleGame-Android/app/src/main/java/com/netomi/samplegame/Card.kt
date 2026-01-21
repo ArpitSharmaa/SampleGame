67be15d10207d7e40746a7edcb6869f15f682766
ArpitSharmaa <arpitskvashista@gmail.com>
2026-01-21 18:12:42 +0530

    Initial commit

--- SampleGame-Android/app/src/main/java/com/netomi/samplegame/Card.kt
+++
@@ -1,9 +0,0 @@
-package com.netomi.samplegame
-
-data class Card(
-    val id: Int,
-    val value: Int,
-    val isFlipped: Boolean = false,
-    val isMatched: Boolean = false
-)
-
--- SampleGame-Android/app/src/main/java/com/netomi/samplegame/GameState.kt
+++
@@ -1,8 +0,0 @@
-package com.netomi.samplegame
-
-data class GameState(
-    val cards: List<Card> = emptyList(),
-    val moves: Int = 0,
-    val isGameComplete: Boolean = false,
-    val canFlipCard: Boolean = true
-)
